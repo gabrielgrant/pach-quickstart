8d082b59bef119a9f998320384c20c34e9756b62
Gabriel Grant <g@briel.ca>
2016-11-27 10:40:43 -0800

    decrease rethinkdb cache size to 16MB

--- launch-pach.sh
+++ launch-pach.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 curl -o /tmp/pachctl.deb -L https://pachyderm.io/pachctl.deb && sudo dpkg -i /tmp/pachctl.deb
-pachctl deploy --rethinkdb-cache-size 256M local
+pachctl deploy --rethinkdb-cache-size 16M local
